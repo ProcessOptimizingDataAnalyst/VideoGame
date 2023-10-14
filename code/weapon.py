fecb1edcfb4641cdff3aa5b6c0e9323b19200891
Alexandra Ngaleba-Ze <89806302+ProcessOptimizingDataAnalyst@users.noreply.github.com>
2023-10-14 11:02:35 -0400

    Update weapon.py

--- code/weapon.py
+++ code/weapon.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Weapon(pygame.sprite.Sprite):
-    def __init__(self,player,groups): # by writing 'player' in the parenthesis we're importing the player
+    def __init__(self,player,groups):
         super().__init__(groups)
         self.sprite_type = 'weapon'
         direction = player.status.split('_')[0]
